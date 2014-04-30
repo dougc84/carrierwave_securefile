02783180a70847297617f42df5518f6069f824f6
mike809 <micha7a.com@gmail.com>
2014-04-29 19:46:01 -0700

    Change hash generator to base64

--- lib/carrierwave/securefile/uploader.rb
+++ lib/carrierwave/securefile/uploader.rb
@@ -7,14 +7,13 @@ module CarrierWave
           ext_file = file + ".x1"
           File.rename(file, ext_file)
           configuration = CarrierWave::SecureFile.configuration
+          debugger;1
           if configuration.encryption_type.downcase.to_sym == :aes
             aes_key = configuration.aes_key
             if model.respond_to? :aes_key and ( model.aes_key.nil? || model.aes_key == '' )
-              digest = Digest::SHA256.new
-              digest.update(file.to_s)
-              model.aes_key = aes_key = digest.to_s
+              model.aes_key = aes_key = SecureRandom.base64(32)
             end
-            unless model.pepper.nil?
+            if model.respond_to?(:pepper) and !model.pepper.nil?
               aes_key = model.pepper
             end
             encryptor = CarrierWave::SecureFile::AESFileEncrypt.new(aes_key, configuration.aes_iv)
